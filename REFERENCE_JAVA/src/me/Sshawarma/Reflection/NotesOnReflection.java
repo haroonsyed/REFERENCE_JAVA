fdb6193e009f4851fa033993d962c27c83449986
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2020-12-27 16:05:58 -0500

    Quick notes on reflection. I doubt I will need to be using it any time soon, since it seems to have niche (and justifiably so) use cases.

---
+++ REFERENCE_JAVA/src/me/Sshawarma/Reflection/NotesOnReflection.java
@@ -0,0 +1,17 @@
+package me.Sshawarma.Reflection;
+
+/*
+
+I may put examples here, but this explains reflection very well:
+https://www.youtube.com/watch?v=agnblS47F18&list=PLF206E906175C7E07&index=10
+
+Essentially it allows you to access any data of a class, or scan for methods and types in the class.
+It is slow, and violates many principles of OOP and access control.
+Use cases are rare, although one example is how JUnit is able to test methods with test annotation.
+
+The api is pretty self explanatory.
+
+ */
+
+public class NotesOnReflection {
+}
